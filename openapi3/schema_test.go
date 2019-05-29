5a634c08d408426d6774933a31137f6d793db309
Yaroslav Zborovsky <yaroslav.zborovsky@gen.tech>
2019-05-29 17:20:12 +0300

    more UUID examples

--- openapi3/schema_test.go
+++ openapi3/schema_test.go
@@ -245,11 +245,15 @@ var schemaExamples = []schemaExample{
 		},
 		AllValid: []interface{}{
 			"dd7d8481-81a3-407f-95f0-a2f1cb382a4b",
+			"dcba3901-2fba-48c1-9db2-00422055804e",
+			"ace8e3be-c254-4c10-8859-1401d9a9d52a",
 		},
 		AllInvalid: []interface{}{
 			nil,
 			"g39840b1-d0ef-446d-e555-48fcca50a90a",
 			"4cf3i040-ea14-4daa-b0b5-ea9329473519",
+			"aaf85740-7e27-4b4f-b4554-a03a43b1f5e3",
+			"56f5bff4-z4b6-48e6-a10d-b6cf66a83b04",
 		},
 	},
 
