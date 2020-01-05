735fa8b63db19535fd2c1547afce193101bdfbf2
James Liu <jaliu@zendesk.com>
2020-01-06 10:27:14 +1100

    feat: add ExtensionProps field to ExternalDocs struct

--- openapi3/external_docs.go
+++ openapi3/external_docs.go
@@ -2,6 +2,8 @@ package openapi3
 
 // ExternalDocs is specified by OpenAPI/Swagger standard version 3.0.
 type ExternalDocs struct {
+	ExtensionProps
+
 	Description string `json:"description,omitempty"`
 	URL         string `json:"url,omitempty"`
 }
