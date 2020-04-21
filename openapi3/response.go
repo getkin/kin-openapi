f72c872e618e2e9bdfa32f7aca1a5fdc3d0b6f09
Francis Lennon <francis.lennon@whitehatsec.com>
2020-04-21 12:33:47 +0100

    Remove omitempty from response description

--- openapi3/response.go
+++ openapi3/response.go
@@ -34,7 +34,7 @@ func (responses Responses) Validate(c context.Context) error {
 // Response is specified by OpenAPI/Swagger 3.0 standard.
 type Response struct {
 	ExtensionProps
-	Description string                `json:"description,omitempty" yaml:"description,omitempty"`
+	Description string                `json:"description" yaml:"description"`
 	Headers     map[string]*HeaderRef `json:"headers,omitempty" yaml:"headers,omitempty"`
 	Content     Content               `json:"content,omitempty" yaml:"content,omitempty"`
 	Links       map[string]*LinkRef   `json:"links,omitempty" yaml:"links,omitempty"`
