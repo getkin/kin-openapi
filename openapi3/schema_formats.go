bcf4aae5a1b9d2960b65d63f9fa33a04c51671c6
Pierre Fenoll <pierrefenoll@gmail.com>
2020-11-12 11:46:34 +0100

    Update openapi3/schema_formats.go

--- openapi3/schema_formats.go
+++ openapi3/schema_formats.go
@@ -11,7 +11,6 @@ const (
 	FormatOfStringForUUIDOfRFC4122 = `^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`
 )
 
-
 //FormatCallback custom check on exotic formats
 type FormatCallback func(Val string) error
 
