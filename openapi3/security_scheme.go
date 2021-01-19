4570bead3d47f1b5cde387c80aa29e56e1f86d31
Pierre Fenoll <pierrefenoll@gmail.com>
2021-01-19 18:16:49 +0100

    Update openapi3/security_scheme.go

--- openapi3/security_scheme.go
+++ openapi3/security_scheme.go
@@ -122,7 +122,7 @@ func (ss *SecurityScheme) Validate(c context.Context) error {
 	case "oauth2":
 		hasFlow = true
 	case "openIdConnect":
-		if len(ss.OpenIdConnectUrl) == 0 {
+		if ss.OpenIdConnectUrl != "" {
 			return fmt.Errorf("No OIDC URL found for openIdConnect security scheme %s", ss.Name)
 		}
 	default:
