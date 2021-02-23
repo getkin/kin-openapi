a3bab3b2faa6922c264088dfd91ac6b006a0d469
Pierre Fenoll <pierrefenoll@gmail.com>
2021-02-23 16:16:44 +0100

    ExampleDecodingExtensionProps
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- openapi3/extension_test.go
+++ openapi3/extension_test.go
@@ -1,11 +1,35 @@
 package openapi3
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/getkin/kin-openapi/jsoninfo"
 	"github.com/stretchr/testify/require"
 )
+
+func ExampleDecodingExtensionProps() {
+	loader := NewSwaggerLoader()
+	loader.IsExternalRefsAllowed = true
+	spec, err := loader.LoadSwaggerFromFile("testdata/testref.openapi.json")
+	if err != nil {
+		panic(err)
+	}
+
+	dec, err := jsoninfo.NewObjectDecoder(spec.Info.Extensions["x-my-extension"].(json.RawMessage))
+	if err != nil {
+		panic(err)
+	}
+	var value struct {
+		Key int `json:"k"`
+	}
+	if err = spec.Info.DecodeWith(dec, &value); err != nil {
+		panic(err)
+	}
+	fmt.Println(value.Key)
+	// Output: 42
+}
 
 func TestExtensionProps_EncodeWith(t *testing.T) {
 	t.Run("successfully encoded", func(t *testing.T) {
