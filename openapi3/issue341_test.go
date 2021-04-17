0531fea523234528c3545b10bb18d34737be9cdd
Pierre Fenoll <pierrefenoll@gmail.com>
2021-04-17 11:50:45 +0200

    repro #341
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

---
+++ openapi3/issue341_test.go
@@ -0,0 +1,26 @@
+package openapi3
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIssue341(t *testing.T) {
+	sl := NewSwaggerLoader()
+	sl.IsExternalRefsAllowed = true
+	doc, err := sl.LoadSwaggerFromFile("testdata/main.yaml")
+	require.NoError(t, err)
+
+	err = doc.Validate(sl.Context)
+	require.NoError(t, err)
+
+	err = sl.ResolveRefsIn(doc, nil)
+	require.NoError(t, err)
+
+	bs, err := doc.MarshalJSON()
+	require.NoError(t, err)
+	t.Logf("serialized: %s", bs)
+
+	require.Equal(t, "string", doc.Paths["/testpath"].Get.Responses["200"].Value.Content["application/json"].Schema.Value.Type)
+}
