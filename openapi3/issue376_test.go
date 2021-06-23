1dc1d38f8ceb43da167c0fe015f34f7b9f8a1b39
Pierre Fenoll <pierrefenoll@gmail.com>
2021-06-23 11:44:02 +0200

    bug: multijson-tagged additionalProperties
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- openapi3/issue376_test.go
+++ openapi3/issue376_test.go
@@ -1,6 +1,7 @@
 package openapi3
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -39,4 +40,54 @@ info:
 	require.Equal(t, 0, len(doc.Paths))
 
 	require.Equal(t, "string", doc.Components.Schemas["schema2"].Value.Properties["prop"].Value.Type)
+}
+
+func TestUnmarshallingMultijsonTag(t *testing.T) {
+	spec := []byte(`
+openapi: 3.0.0
+components:
+  schemas:
+    unset:
+      type: number
+    empty-object:
+      additionalProperties: {}
+    object:
+      additionalProperties: {type: string}
+    boolean:
+      additionalProperties: false
+paths: {}
+info:
+  title: An API
+  version: 1.2.3.4
+`)
+
+	loader := NewLoader()
+
+	doc, err := loader.LoadFromData(spec)
+	require.NoError(t, err)
+
+	err = doc.Validate(loader.Context)
+	require.NoError(t, err)
+
+	for propName, propSchema := range doc.Components.Schemas {
+		ap := propSchema.Value.AdditionalProperties
+		apa := propSchema.Value.AdditionalPropertiesAllowed
+
+		if propName == "unset" {
+			require.True(t, ap == nil && apa == nil)
+			continue
+		}
+
+		apStr := ""
+		if ap != nil {
+			apStr = fmt.Sprintf("{Ref:- Value.AdditionalProperties:%+v Value.AdditionalPropertiesAllowed:%+v}", (*ap).Value.AdditionalProperties, (*ap).Value.AdditionalPropertiesAllowed)
+		}
+		apaStr := ""
+		if apa != nil {
+			apaStr = fmt.Sprintf("%v", *apa)
+		}
+
+		require.Truef(t, (ap != nil && apa == nil) || (ap == nil && apa != nil),
+			"%s: isnil(%s) xor isnil(%s)", propName, apaStr, apStr)
+	}
 }
