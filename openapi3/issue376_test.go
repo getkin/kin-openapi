d30837871e4548501ae7e3fc9ed0f74e35caa53d
Pierre Fenoll <pierrefenoll@gmail.com>
2021-06-23 13:45:59 +0200

    fix drilling down additionalProperties in the boolean case (#378)

--- openapi3/issue376_test.go
+++ openapi3/issue376_test.go
@@ -1,6 +1,7 @@
 package openapi3
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -39,4 +40,63 @@ info:
 	require.Equal(t, 0, len(doc.Paths))
 
 	require.Equal(t, "string", doc.Components.Schemas["schema2"].Value.Properties["prop"].Value.Type)
+}
+
+func TestMultijsonTagSerialization(t *testing.T) {
+	spec := []byte(`
+openapi: 3.0.0
+components:
+  schemas:
+    unset:
+      type: number
+    #empty-object:
+    # TODO additionalProperties: {}
+    object:
+      additionalProperties: {type: string}
+    boolean:
+      additionalProperties: false
+paths: {}
+info:
+  title: An API
+  version: 1.2.3.4
+`)
+
+	loader := NewLoader()
+
+	doc, err := loader.LoadFromData(spec)
+	require.NoError(t, err)
+
+	err = doc.Validate(loader.Context)
+	require.NoError(t, err)
+
+	for propName, propSchema := range doc.Components.Schemas {
+		ap := propSchema.Value.AdditionalProperties
+		apa := propSchema.Value.AdditionalPropertiesAllowed
+
+		encoded, err := propSchema.MarshalJSON()
+		require.NoError(t, err)
+		require.Equal(t, string(encoded), map[string]string{
+			"unset": `{"type":"number"}`,
+			// TODO: "empty-object":`{"additionalProperties":{}}`,
+			"object":  `{"additionalProperties":{"type":"string"}}`,
+			"boolean": `{"additionalProperties":false}`,
+		}[propName])
+
+		if propName == "unset" {
+			require.True(t, ap == nil && apa == nil)
+			continue
+		}
+
+		apStr := ""
+		if ap != nil {
+			apStr = fmt.Sprintf("{Ref:%s Value.Type:%v}", (*ap).Ref, (*ap).Value.Type)
+		}
+		apaStr := ""
+		if apa != nil {
+			apaStr = fmt.Sprintf("%v", *apa)
+		}
+
+		require.Truef(t, (ap != nil && apa == nil) || (ap == nil && apa != nil),
+			"%s: isnil(%s) xor isnil(%s)", propName, apaStr, apStr)
+	}
 }
