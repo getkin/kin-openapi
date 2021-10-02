83f14b966911c043ba2215b4d573310596c652c8
Pierre Fenoll <pierrefenoll@gmail.com>
2021-10-02 12:20:42 +0200

    right
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- openapi3/schema_formats_test.go
+++ openapi3/schema_formats_test.go
@@ -1,20 +1,27 @@
-package openapi3
+package openapi3_test
 
 import (
 	"context"
 	"testing"
 
+	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/stretchr/testify/require"
 )
 
 func TestIssue430(t *testing.T) {
-	schema := NewOneOfSchema(
-		NewStringSchema().WithFormat("ipv4"),
-		NewStringSchema().WithFormat("ipv6"),
+	schema := openapi3.NewOneOfSchema(
+		openapi3.NewStringSchema().WithFormat("ipv4"),
+		openapi3.NewStringSchema().WithFormat("ipv6"),
 	)
 
 	err := schema.Validate(context.Background())
 	require.NoError(t, err)
+
+	err = schema.VisitJSON("127.0.1.1")
+	require.Error(t, err, openapi3.ErrOneOfConflict.Error())
+
+	openapi3.DefineIPv4Format()
+	openapi3.DefineIPv6Format()
 
 	err = schema.VisitJSON("127.0.1.1")
 	require.NoError(t, err)
