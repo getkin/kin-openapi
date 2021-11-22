3ad289823011c4714a9e17846b85dbeda9c8984d
Andrey Dyatlov <adyatlov@mesosphere.io>
2021-11-22 12:08:49 +0100

    Fix indentation.

--- routers/gorillamux/router.go
+++ routers/gorillamux/router.go
@@ -45,9 +45,9 @@ func NewRouter(doc *openapi3.T) (routers.Router, error) {
 		} else {
 			u, err = url.Parse(bEncode(serverURL))
 		}
-	  if err != nil {
-		  return nil, err
-	  }
+		if err != nil {
+			return nil, err
+		}
 		path := bDecode(u.EscapedPath())
 		if len(path) > 0 && path[len(path)-1] == '/' {
 			path = path[:len(path)-1]
