464e0cb94f5f1aa93443082b970363f16d3ae915
Kanai Masumi <m-kanai@bbsakura.net>
2022-08-29 15:50:28 +0900

    test: add assertion to load_cicular_ref_with_external_file_test

--- openapi3/load_cicular_ref_with_external_file_test.go
+++ openapi3/load_cicular_ref_with_external_file_test.go
@@ -5,9 +5,10 @@ package openapi3_test
 
 import (
 	"embed"
-	"fmt"
 	"net/url"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 
 	"github.com/getkin/kin-openapi/openapi3"
 )
@@ -22,9 +23,60 @@ func TestLoadCircularRefFromFile(t *testing.T) {
 		return circularResSpecs.ReadFile(uri.Path)
 	}
 
-	doc, err := loader.LoadFromFile("testdata/circularRef/base.yml")
+	got, err := loader.LoadFromFile("testdata/circularRef/base.yml")
 	if err != nil {
 		t.Error(err)
 	}
-	fmt.Printf("%v\n", doc)
+
+	foo := &openapi3.SchemaRef{
+		Ref: "",
+		Value: &openapi3.Schema{
+			ExtensionProps: openapi3.ExtensionProps{Extensions: map[string]interface{}{}},
+			Properties: map[string]*openapi3.SchemaRef{
+				"foo2": { // reference to an external file
+					Ref: "other.yml#/components/schemas/Foo2",
+					Value: &openapi3.Schema{
+						ExtensionProps: openapi3.ExtensionProps{Extensions: map[string]interface{}{}},
+						Properties: map[string]*openapi3.SchemaRef{
+							"id": {
+								Value: &openapi3.Schema{
+									Type:           "string",
+									ExtensionProps: openapi3.ExtensionProps{Extensions: map[string]interface{}{}},
+								}},
+						},
+					},
+				},
+			},
+		},
+	}
+	bar := &openapi3.SchemaRef{
+		Ref: "",
+		Value: &openapi3.Schema{
+			ExtensionProps: openapi3.ExtensionProps{Extensions: map[string]interface{}{}},
+			Properties:     map[string]*openapi3.SchemaRef{},
+		},
+	}
+	// circular reference
+	bar.Value.Properties["foo"] = &openapi3.SchemaRef{Ref: "#/components/schemas/Foo", Value: foo.Value}
+	foo.Value.Properties["bar"] = &openapi3.SchemaRef{Ref: "#/components/schemas/Bar", Value: bar.Value}
+
+	want := &openapi3.T{
+		OpenAPI: "3.0.3",
+		Info: &openapi3.Info{
+			Title:   "Recursive cyclic refs example",
+			Version: "1.0",
+
+			ExtensionProps: openapi3.ExtensionProps{Extensions: map[string]interface{}{}},
+		},
+		Components: openapi3.Components{
+			ExtensionProps: openapi3.ExtensionProps{Extensions: map[string]interface{}{}},
+			Schemas: map[string]*openapi3.SchemaRef{
+				"Foo": foo,
+				"Bar": bar,
+			},
+		},
+		ExtensionProps: openapi3.ExtensionProps{Extensions: map[string]interface{}{}},
+	}
+
+	require.Equal(t, want, got)
 }
