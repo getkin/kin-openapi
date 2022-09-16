6be2e62872f982b00c4552f4d902552c74a4c14a
Pierre Fenoll <pierrefenoll@gmail.com>
2022-09-16 11:15:44 +0200

    Remove unused argument

--- openapi3/example_validation.go
+++ openapi3/example_validation.go
@@ -1,9 +1,5 @@
 package openapi3
 
-import (
-	"context"
-)
-
-func validateExampleValue(ctx context.Context, input interface{}, schema *Schema) error {
+func validateExampleValue(input interface{}, schema *Schema) error {
 	return schema.VisitJSON(input, MultiErrors())
 }
