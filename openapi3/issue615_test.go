ae20d7ae81702ce0c07a56787a9b7e57ce236bd8
Pierre Fenoll <pierrefenoll@gmail.com>
2022-10-07 18:27:20 +0200

    ensure test fails successfully
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- openapi3/issue615_test.go
+++ openapi3/issue615_test.go
@@ -9,19 +9,24 @@ import (
 )
 
 func TestIssue615(t *testing.T) {
-	loader := openapi3.NewLoader()
-	loader.IsExternalRefsAllowed = true
-	_, err := loader.LoadFromFile("testdata/recursiveRef/issue615.yml")
-	// Test currently reproduces the issue 615: failure to load a valid spec
-	// Upon issue resolution, this check should be changed to require.NoError
-	require.Error(t, err, openapi3.CircularReferenceError)
+	for {
+		loader := openapi3.NewLoader()
+		loader.IsExternalRefsAllowed = true
+		_, err := loader.LoadFromFile("testdata/recursiveRef/issue615.yml")
+		// Test currently reproduces the issue 615: failure to load a valid spec
+		// Upon issue resolution, this check should be changed to require.NoError
+		require.Error(t, err, openapi3.CircularReferenceError)
+		if err != nil {
+			break
+		}
+	}
 
 	var old int
 	old, openapi3.CircularReferenceCounter = openapi3.CircularReferenceCounter, 4
 	defer func() { openapi3.CircularReferenceCounter = old }()
 
-	loader = openapi3.NewLoader()
+	loader := openapi3.NewLoader()
 	loader.IsExternalRefsAllowed = true
-	_, err = loader.LoadFromFile("testdata/recursiveRef/issue615.yml")
+	_, err := loader.LoadFromFile("testdata/recursiveRef/issue615.yml")
 	require.NoError(t, err)
 }
