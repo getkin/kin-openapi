5d74c2df9ed6a1be69527dbf7092fcf79787d038
dominicqi <dominic.qiyang@gmail.com>
2022-11-28 11:52:15 +0800

    fix: example ref to schemas cause error unit test

--- openapi3/loader_exmaple_schema_ref_test.go
+++ openapi3/loader_exmaple_schema_ref_test.go
@@ -8,10 +8,13 @@ import (
 
 func TestLoadSpecWithExampleRefToSchemas(t *testing.T) {
 	loader := NewLoader()
-	loader.IsExternalRefsAllowed = true
-	api, err := loader.LoadFromFile("testdata/example-schemas-ref.json")
-	exampleValue := api.Paths["/pet"].Put.Responses.Get(400).Value.Content.Get("*/*").Examples["200"].Value.Value
-	schemaRef := api.Components.Schemas["Pet"].Value
+	doc, err := loader.LoadFromFile("testdata/example-schemas-ref.json")
+	require.NoError(t, err)
+
+	err = doc.Validate(loader.Context)
+	require.NoError(t, err)
+	exampleValue := doc.Paths["/pet"].Put.Responses.Get(400).Value.Content.Get("*/*").Examples["200"].Value.Value
+	schemaRef := doc.Components.Schemas["Pet"].Value
 	require.Equal(t, exampleValue, schemaRef)
 	require.NoError(t, err)
 }
