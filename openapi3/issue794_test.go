4f5903d557960119d62355109190db32843682e8
Reuven <rh@tufin.com>
2023-05-29 19:54:30 +0300

    fix comment

--- openapi3/issue794_test.go
+++ openapi3/issue794_test.go
@@ -8,6 +8,8 @@ import (
 
 func TestCrashOnLoad(t *testing.T) {
 	loader := NewLoader()
-	_, err := loader.LoadFromFile("testdata/issue794.yml")
+	doc, err := loader.LoadFromFile("testdata/issue794.yml")
+	require.NoError(t, err)
+	err = doc.Validate(loader.Context)
 	require.NoError(t, err)
 }
