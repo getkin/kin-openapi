9c0054f4002f63bd526a3a7806c81c739a880538
Christian Boitel <christian.boitel@gmail.com>
2023-07-31 13:32:21 +0200

    feat #582: add minor version support to string formats

--- openapi3/schema_formats.go
+++ openapi3/schema_formats.go
@@ -21,26 +21,99 @@ type FormatCallback func(value string) error
 
 // Format represents a format validator registered by either DefineStringFormat or DefineStringFormatCallback
 type Format struct {
+	versionedFormats []*versionedFormat
+}
+
+type versionedFormat struct {
 	regexp   *regexp.Regexp
 	callback FormatCallback
 }
 
+func (format *Format) add(minMinorVersion uint64, vFormat *versionedFormat) {
+	if format != nil {
+		if format.versionedFormats == nil {
+			format.versionedFormats = make([]*versionedFormat, minMinorVersion+1)
+			format.versionedFormats[minMinorVersion] = vFormat
+		} else {
+			numVersionedFormats := uint64(len(format.versionedFormats))
+			if minMinorVersion >= numVersionedFormats {
+				// grow array
+				lastValue := format.versionedFormats[numVersionedFormats-1]
+				additionalEntries := make([]*versionedFormat, minMinorVersion+1-numVersionedFormats)
+				if lastValue != nil {
+					for i := 0; i < len(additionalEntries); i++ {
+						additionalEntries[i] = lastValue
+					}
+				}
+				format.versionedFormats = append(format.versionedFormats, additionalEntries...)
+				format.versionedFormats[minMinorVersion] = vFormat
+				return
+			}
+			for i := minMinorVersion; i < numVersionedFormats; i++ {
+				format.versionedFormats[i] = vFormat
+			}
+		}
+	}
+}
+
+func (format Format) get(minorVersion uint64) *versionedFormat {
+	if format.versionedFormats != nil {
+		if minorVersion >= uint64(len(format.versionedFormats)) {
+			return format.versionedFormats[len(format.versionedFormats)-1]
+		}
+		return format.versionedFormats[minorVersion]
+	}
+	return nil
+}
+
+func (format Format) DefinedForMinorVersion(minorVersion uint64) bool {
+	return format.get(minorVersion) != nil
+}
+
 // SchemaStringFormats allows for validating string formats
-var SchemaStringFormats = make(map[string]Format, 4)
+var SchemaStringFormats = make(map[string]*Format, 4)
+var defaultSchemaStringFormats map[string]*Format
 
 // DefineStringFormat defines a new regexp pattern for a given format
-func DefineStringFormat(name string, pattern string) {
+// Will enforce regexp usage for minor versions of OpenAPI (3.Y.Z)
+func DefineStringFormat(name string, pattern string, options ...SchemaFormatOption) {
+	var schemaFormatOptions SchemaFormatOptions
+	for _, option := range options {
+		option(&schemaFormatOptions)
+	}
 	re, err := regexp.Compile(pattern)
 	if err != nil {
 		err := fmt.Errorf("format %q has invalid pattern %q: %w", name, pattern, err)
 		panic(err)
 	}
-	SchemaStringFormats[name] = Format{regexp: re}
+	updateSchemaStringFormats(name, schemaFormatOptions.fromOpenAPIMinorVersion, &versionedFormat{regexp: re})
+}
+
+func getSchemaStringFormats(name string, minorVersion uint64) *versionedFormat {
+	if currentStringFormat, found := SchemaStringFormats[name]; found {
+		return currentStringFormat.get(minorVersion)
+	}
+	return nil
+}
+
+func updateSchemaStringFormats(name string, minMinorVersion uint64, vFormat *versionedFormat) {
+	if currentStringFormat, found := SchemaStringFormats[name]; found {
+		currentStringFormat.add(minMinorVersion, vFormat)
+		return
+	}
+	var newFormat Format
+	newFormat.add(minMinorVersion, vFormat)
+	SchemaStringFormats[name] = &newFormat
 }
 
 // DefineStringFormatCallback adds a validation function for a specific schema format entry
-func DefineStringFormatCallback(name string, callback FormatCallback) {
-	SchemaStringFormats[name] = Format{callback: callback}
+// Will enforce regexp usage for minor versions of OpenAPI (3.Y.Z)
+func DefineStringFormatCallback(name string, callback FormatCallback, options ...SchemaFormatOption) {
+	var schemaFormatOptions SchemaFormatOptions
+	for _, option := range options {
+		option(&schemaFormatOptions)
+	}
+	updateSchemaStringFormats(name, schemaFormatOptions.fromOpenAPIMinorVersion, &versionedFormat{callback: callback})
 }
 
 func validateIP(ip string) error {
@@ -82,6 +155,51 @@ func validateIPv6(ip string) error {
 	return nil
 }
 
+// SaveStringFormats allows to save (obtain a deep copy) of your current string formats
+// so you can later restore it if needed
+func SaveStringFormats(map[string]*Format) map[string]*Format {
+	savedStringFormats := map[string]*Format{}
+	for name, value := range SchemaStringFormats {
+		var savedFormat Format
+		savedFormat.versionedFormats = make([]*versionedFormat, len(value.versionedFormats))
+		for index, versionedFormatValue := range value.versionedFormats {
+			if versionedFormatValue != nil {
+				savedVersionedFormat := versionedFormat{
+					regexp:   versionedFormatValue.regexp,
+					callback: versionedFormatValue.callback,
+				}
+				savedFormat.versionedFormats[index] = &savedVersionedFormat
+			}
+		}
+		savedStringFormats[name] = &savedFormat
+	}
+	return savedStringFormats
+}
+
+// RestoreStringFormats allows to restore string format back to default values
+func RestoreStringFormats(formatToRestore map[string]*Format) {
+	restoredStringFormats := map[string]*Format{}
+	for name, value := range formatToRestore {
+		var restoredFormat Format
+		restoredFormat.versionedFormats = make([]*versionedFormat, len(value.versionedFormats))
+		for index, versionedFormatValue := range value.versionedFormats {
+			if versionedFormatValue != nil {
+				restoredVersionedFormat := versionedFormat{
+					regexp:   versionedFormatValue.regexp,
+					callback: versionedFormatValue.callback,
+				}
+				restoredFormat.versionedFormats[index] = &restoredVersionedFormat
+			}
+		}
+		restoredStringFormats[name] = &restoredFormat
+	}
+	SchemaStringFormats = restoredStringFormats
+}
+
+// RestoreDefaultStringFormats allows to restore string format back to default values
+func RestoreDefaultStringFormats() {
+	RestoreStringFormats(defaultSchemaStringFormats)
+}
 func init() {
 	// Base64
 	// The pattern supports base64 and b./ase64url. Padding ('=') is supported.
@@ -93,6 +211,7 @@ func init() {
 	// date-time
 	DefineStringFormat("date-time", `^[0-9]{4}-(0[0-9]|10|11|12)-([0-2][0-9]|30|31)T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?(Z|(\+|-)[0-9]{2}:[0-9]{2})?$`)
 
+	defaultSchemaStringFormats = SaveStringFormats(SchemaStringFormats)
 }
 
 // DefineIPv4Format opts in ipv4 format validation on top of OAS 3 spec
--- openapi3/schema_formats_test.go
+++ openapi3/schema_formats_test.go
@@ -16,9 +16,6 @@ func TestIssue430(t *testing.T) {
 		NewStringSchema().WithFormat("ipv6"),
 	)
 
-	delete(SchemaStringFormats, "ipv4")
-	delete(SchemaStringFormats, "ipv6")
-
 	err := schema.Validate(context.Background())
 	require.NoError(t, err)
 
@@ -46,11 +43,8 @@ func TestIssue430(t *testing.T) {
 		require.Error(t, err, ErrOneOfConflict.Error())
 	}
 
-	DefineIPv4Format()
-	DefineIPv6Format()
-
 	for datum, isV4 := range data {
-		err = schema.VisitJSON(datum)
+		err = schema.VisitJSON(datum, SetOpenAPIMinorVersion(1))
 		require.NoError(t, err)
 		if isV4 {
 			require.Nil(t, validateIPv4(datum), "%q should be IPv4", datum)
@@ -78,8 +72,6 @@ func TestFormatCallback_WrapError(t *testing.T) {
 }
 
 func TestReversePathInMessageSchemaError(t *testing.T) {
-	DefineIPv4Format()
-
 	SchemaErrorDetailsDisabled = true
 
 	const spc = `
@@ -99,11 +91,11 @@ components:
 
 	err = doc.Components.Schemas["Something"].Value.VisitJSON(map[string]interface{}{
 		`ip`: `123.0.0.11111`,
-	})
+	}, SetOpenAPIMinorVersion(1))
 
-	require.EqualError(t, err, `Error at "/ip": Not an IP address`)
+	// assert, do not require to ensure SchemaErrorDetailsDisabled can be set to false
+	assert.ErrorContains(t, err, `Error at "/ip"`)
 
-	delete(SchemaStringFormats, "ipv4")
 	SchemaErrorDetailsDisabled = false
 }
 
@@ -116,6 +108,7 @@ func TestUuidFormat(t *testing.T) {
 	}
 
 	DefineStringFormat("uuid", FormatOfStringForUUIDOfRFC4122)
+	defer RestoreDefaultStringFormats()
 	testCases := []testCase{
 		{
 			name:    "invalid",
@@ -152,5 +145,263 @@ func TestUuidFormat(t *testing.T) {
 				require.Nil(t, err)
 			}
 		})
+	}
+}
+func TestStringFormatsStartingWithOpenAPIMinorVersion(t *testing.T) {
+	DefineStringFormat("test", "test0", FromOpenAPIMinorVersion(0))
+	defer RestoreDefaultStringFormats()
+	for i := uint64(0); i < 10; i++ {
+		if assert.Contains(t, SchemaStringFormats, "test") &&
+			assert.NotNilf(t, SchemaStringFormats["test"].get(i), "%d", i) {
+			if assert.NotNilf(t, SchemaStringFormats["test"].get(i).regexp, "%d", i) {
+				assert.Equalf(t, "test0", SchemaStringFormats["test"].get(i).regexp.String(), "%d", i)
+			}
+			assert.Nilf(t, SchemaStringFormats["test"].get(i).callback, "%d", i)
+		}
+	}
+
+	DefineStringFormat("test", "test1", FromOpenAPIMinorVersion(1))
+	for i := uint64(0); i < 10; i++ {
+		if assert.Contains(t, SchemaStringFormats, "test") &&
+			assert.NotNilf(t, SchemaStringFormats["test"].get(i), "%d", i) {
+			if assert.NotNilf(t, SchemaStringFormats["test"].get(i).regexp, "%d", i) {
+				var regexpString string
+				switch {
+				case i == 0:
+					regexpString = "test0"
+				case i > 0:
+					regexpString = "test1"
+				}
+				assert.Equalf(t, regexpString, SchemaStringFormats["test"].get(i).regexp.String(), "%d", i)
+			}
+			assert.Nilf(t, SchemaStringFormats["test"].get(i).callback, "%d", i)
+		}
+	}
+
+	DefineStringFormat("test", "test5", FromOpenAPIMinorVersion(5))
+	for i := uint64(0); i < 10; i++ {
+		if assert.Contains(t, SchemaStringFormats, "test") &&
+			assert.NotNilf(t, SchemaStringFormats["test"].get(i), "%d", i) {
+			if assert.NotNilf(t, SchemaStringFormats["test"].get(i).regexp, "%d", i) {
+				var regexpString string
+				switch {
+				case i == 0:
+					regexpString = "test0"
+				case i >= 1 && i < 5:
+					regexpString = "test1"
+				case i >= 5:
+					regexpString = "test5"
+				}
+				assert.Equalf(t, regexpString, SchemaStringFormats["test"].get(i).regexp.String(), "%d", i)
+			}
+			assert.Nilf(t, SchemaStringFormats["test"].get(i).callback, "%d", i)
+		}
+	}
+
+	DefineStringFormat("test", "test2", FromOpenAPIMinorVersion(2))
+	for i := uint64(0); i < 10; i++ {
+		if assert.Contains(t, SchemaStringFormats, "test") &&
+			assert.NotNilf(t, SchemaStringFormats["test"].get(i), "%d", i) {
+			if assert.NotNilf(t, SchemaStringFormats["test"].get(i).regexp, "%d", i) {
+				var regexpString string
+				switch {
+				case i == 0:
+					regexpString = "test0"
+				case i == 1:
+					regexpString = "test1"
+				case i >= 2:
+					regexpString = "test2"
+				}
+				assert.Equalf(t, regexpString, SchemaStringFormats["test"].get(i).regexp.String(), "%d", i)
+			}
+			assert.Nilf(t, SchemaStringFormats["test"].get(i).callback, "%d", i)
+		}
+	}
+
+	DefineStringFormat("test", "test4", FromOpenAPIMinorVersion(4))
+	for i := uint64(0); i < 10; i++ {
+		if assert.Contains(t, SchemaStringFormats, "test") &&
+			assert.NotNilf(t, SchemaStringFormats["test"].get(i), "%d", i) {
+			if assert.NotNilf(t, SchemaStringFormats["test"].get(i).regexp, "%d", i) {
+				var regexpString string
+				switch {
+				case i == 0:
+					regexpString = "test0"
+				case i == 1:
+					regexpString = "test1"
+				case i >= 2 && i < 4:
+					regexpString = "test2"
+				case i >= 4:
+					regexpString = "test4"
+				}
+				assert.Equalf(t, regexpString, SchemaStringFormats["test"].get(i).regexp.String(), "%d", i)
+			}
+			assert.Nilf(t, SchemaStringFormats["test"].get(i).callback, "%d", i)
+		}
+	}
+
+	DefineStringFormat("test", "test3", FromOpenAPIMinorVersion(3))
+	for i := uint64(0); i < 10; i++ {
+		if assert.Contains(t, SchemaStringFormats, "test") &&
+			assert.NotNilf(t, SchemaStringFormats["test"].get(i), "%d", i) {
+			if assert.NotNilf(t, SchemaStringFormats["test"].get(i).regexp, "%d", i) {
+				var regexpString string
+				switch {
+				case i == 0:
+					regexpString = "test0"
+				case i == 1:
+					regexpString = "test1"
+				case i == 2:
+					regexpString = "test2"
+				case i >= 3:
+					regexpString = "test3"
+				}
+				assert.Equalf(t, regexpString, SchemaStringFormats["test"].get(i).regexp.String(), "%d", i)
+			}
+			assert.Nilf(t, SchemaStringFormats["test"].get(i).callback, "%d", i)
+		}
+	}
+
+	DefineStringFormat("test", "test7", FromOpenAPIMinorVersion(7))
+	for i := uint64(0); i < 10; i++ {
+		if assert.Contains(t, SchemaStringFormats, "test") &&
+			assert.NotNilf(t, SchemaStringFormats["test"].get(i), "%d", i) {
+			if assert.NotNilf(t, SchemaStringFormats["test"].get(i).regexp, "%d", i) {
+				var regexpString string
+				switch {
+				case i == 0:
+					regexpString = "test0"
+				case i == 1:
+					regexpString = "test1"
+				case i == 2:
+					regexpString = "test2"
+				case i >= 3 && i < 7:
+					regexpString = "test3"
+				case i >= 7:
+					regexpString = "test7"
+				}
+				assert.Equalf(t, regexpString, SchemaStringFormats["test"].get(i).regexp.String(), "%d", i)
+			}
+			assert.Nilf(t, SchemaStringFormats["test"].get(i).callback, "%d", i)
+		}
+	}
+
+	DefineStringFormat("test", "testnew")
+	for i := uint64(0); i < 10; i++ {
+		if assert.Contains(t, SchemaStringFormats, "test") &&
+			assert.NotNilf(t, SchemaStringFormats["test"].get(i), "%d", i) {
+			if assert.NotNilf(t, SchemaStringFormats["test"].get(i).regexp, "%d", i) {
+				assert.Equalf(t, "testnew", SchemaStringFormats["test"].get(i).regexp.String(), "%d", i)
+			}
+			assert.Nilf(t, SchemaStringFormats["test"].get(i).callback, "%d", i)
+		}
+	}
+}
+
+func createCallBackError(minorVersion uint) error {
+	return fmt.Errorf("%d", minorVersion)
+}
+func createCallBack(callbackError error) FormatCallback {
+	return func(name string) error {
+		return callbackError
+	}
+}
+
+func TestStringFormatsCallbackStartingWithOpenAPIMinorVersion(t *testing.T) {
+	defer RestoreDefaultStringFormats()
+	callbackError0 := createCallBackError(0)
+	DefineStringFormatCallback("testCallback", createCallBack(callbackError0))
+	for i := uint64(0); i < 10; i++ {
+		if assert.NotNilf(t, SchemaStringFormats["testCallback"], "%d", i) &&
+			assert.NotNilf(t, SchemaStringFormats["testCallback"].get(i), "%d", i) {
+			assert.Emptyf(t, SchemaStringFormats["testCallback"].get(i).regexp, "%d", i)
+			assert.Equal(t, callbackError0, SchemaStringFormats["testCallback"].get(i).callback("ignored"), "%d", i)
+		}
+	}
+
+	callbackError1 := createCallBackError(1)
+	DefineStringFormatCallback("testCallback", createCallBack(callbackError1), FromOpenAPIMinorVersion(1))
+	for i := uint64(0); i < 10; i++ {
+		if assert.NotNilf(t, SchemaStringFormats["testCallback"], "%d", i) &&
+			assert.NotNilf(t, SchemaStringFormats["testCallback"].get(i), "%d", i) {
+			assert.Emptyf(t, SchemaStringFormats["testCallback"].get(i).regexp, "%d", i)
+			var err error
+			switch {
+			case i == 0:
+				err = callbackError0
+			case i > 0:
+				err = callbackError1
+			}
+			assert.Equal(t, err, SchemaStringFormats["testCallback"].get(i).callback("ignored"), "%d", i)
+		}
+	}
+	callbackError5 := createCallBackError(5)
+	DefineStringFormatCallback("testCallback", createCallBack(callbackError5), FromOpenAPIMinorVersion(5))
+	assert.Equal(t, 6, len(SchemaStringFormats["testCallback"].versionedFormats))
+	for i := uint64(0); i < 10; i++ {
+		if assert.NotNilf(t, SchemaStringFormats["testCallback"], "%d", i) &&
+			assert.NotNilf(t, SchemaStringFormats["testCallback"].get(i), "%d", i) {
+			assert.Emptyf(t, SchemaStringFormats["testCallback"].get(i).regexp, "%d", i)
+			var err error
+			switch {
+			case i == 0:
+				err = callbackError0
+			case i > 0 && i < 5:
+				err = callbackError1
+			case i >= 5:
+				err = callbackError5
+			}
+			assert.Equal(t, err, SchemaStringFormats["testCallback"].get(i).callback("ignored"), "%d", i)
+		}
+	}
+	callbackError3 := createCallBackError(3)
+	DefineStringFormatCallback("testCallback", createCallBack(callbackError3), FromOpenAPIMinorVersion(3))
+	assert.Equal(t, 6, len(SchemaStringFormats["testCallback"].versionedFormats))
+	for i := uint64(0); i < 10; i++ {
+		if assert.NotNilf(t, SchemaStringFormats["testCallback"], "%d", i) &&
+			assert.NotNilf(t, SchemaStringFormats["testCallback"].get(i), "%d", i) {
+			assert.Emptyf(t, SchemaStringFormats["testCallback"].get(i).regexp, "%d", i)
+			var err error
+			switch {
+			case i == 0:
+				err = callbackError0
+			case i > 0 && i < 3:
+				err = callbackError1
+			case i >= 3:
+				err = callbackError3
+			}
+			assert.Equal(t, err, SchemaStringFormats["testCallback"].get(i).callback("ignored"), "%d", i)
+		}
+	}
+	callbackError4 := createCallBackError(4)
+	DefineStringFormatCallback("testCallback", createCallBack(callbackError4), FromOpenAPIMinorVersion(4))
+	assert.Equal(t, 6, len(SchemaStringFormats["testCallback"].versionedFormats))
+	for i := uint64(0); i < 10; i++ {
+		if assert.NotNilf(t, SchemaStringFormats["testCallback"], "%d", i) &&
+			assert.NotNilf(t, SchemaStringFormats["testCallback"].get(i), "%d", i) {
+			assert.Emptyf(t, SchemaStringFormats["testCallback"].get(i).regexp, "%d", i)
+			var err error
+			switch {
+			case i == 0:
+				err = callbackError0
+			case i > 0 && i < 3:
+				err = callbackError1
+			case i == 3:
+				err = callbackError3
+			case i >= 4:
+				err = callbackError4
+			}
+			assert.Equal(t, err, SchemaStringFormats["testCallback"].get(i).callback("ignored"), "%d", i)
+		}
+	}
+	callbackError99 := createCallBackError(99)
+	DefineStringFormatCallback("testCallback", createCallBack(callbackError99))
+	assert.Equal(t, 6, len(SchemaStringFormats["testCallback"].versionedFormats))
+	for i := uint64(0); i < 10; i++ {
+		if assert.NotNilf(t, SchemaStringFormats["testCallback"], "%d", i) &&
+			assert.NotNilf(t, SchemaStringFormats["testCallback"].get(i), "%d", i) {
+			assert.Emptyf(t, SchemaStringFormats["testCallback"].get(i).regexp, "%d", i)
+			assert.Equal(t, callbackError99, SchemaStringFormats["testCallback"].get(i).callback("ignored"), "%d", i)
+		}
 	}
 }
