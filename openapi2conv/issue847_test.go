4719b4efc4eebaca3e0ad0ff38411919de1d30a3
Eric Yue <yuekewin@gmail.com>
2023-10-18 12:22:05 +0800

    perf: update issue847_test, add expect result

--- openapi2conv/issue847_test.go
+++ openapi2conv/issue847_test.go
@@ -2,59 +2,42 @@ package openapi2conv
 
 import (
 	"context"
-	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestIssue847(t *testing.T) {
-	spec := []byte(`
-{
-  "swagger": "2.0",
-  "info": {
-    "description": "Hello Java Sec API",
-    "version": "1.10",
-    "title": "Swagger2 RESTful API"
-  },
-  "host": "localhost:8888",
-  "basePath": "/",
-  "paths": {
-    "/UPLOAD/uploadSafe": {
-      "post": {
-        "operationId": "singleFileUploadSafeUsingPOST",
-        "consumes": [
-          "multipart/form-data"
-        ],
-        "parameters": [
-          {
-            "name": "file",
-            "in": "formData",
-            "description": "file",
-            "required": true,
-            "type": "file"
-          }
-        ],
-        "responses": {
-          "200": {
-            "description": "OK",
-            "schema": {
-              "type": "string"
-            }
-          }
-        }
-      }
-    }
-  }
-}
+	v2 := []byte(`
+swagger: '2.0'
+info:
+  version: '1.10'
+  title: title
+paths:
+  "/ping":
+    post:
+      consumes:
+      - multipart/form-data
+      parameters:
+      - name: file
+        in: formData
+        description: file
+        required: true
+        type: file
+      responses:
+        '200':
+          description: OK
 `)
-	doc3, err := v2v3JSON(spec)
+
+	v3, err := v2v3YAML(v2)
 	require.NoError(t, err)
 
-	reqStr, err := json.MarshalIndent(doc3, "", "    ")
-	require.NotNil(t, reqStr)
+	err = v3.Validate(context.Background())
 	require.NoError(t, err)
 
-	err = doc3.Validate(context.Background())
-	require.NoError(t, err)
+	schemaRequired := v3.Paths["/ping"].Post.RequestBody.Value.Content["multipart/form-data"].Schema.Value.Required
+	require.Equal(t, schemaRequired, []string{"file"})
+
+	fieldRequired := v3.Paths["/ping"].Post.RequestBody.Value.Content["multipart/form-data"].Schema.Value.Properties["file"].Value.Required
+	require.Nil(t, fieldRequired)
 }
