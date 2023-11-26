10fb81dee941ce794b577aab8c7b367819a1181d
Pierre Fenoll <pierrefenoll@gmail.com>
2023-11-26 19:29:46 +0100

    openapi3: support \uC4FE codepoint syntax in Schema.Pattern
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

---
+++ openapi3/schema_pattern_test.go
@@ -0,0 +1,18 @@
+package openapi3
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPattern(t *testing.T) {
+	_, err := regexp.Compile("^[a-zA-Z\\u0080-\\u024F\\s\\/\\-\\)\\(\\`\\.\\\"\\']+$")
+	require.EqualError(t, err, "error parsing regexp: invalid escape sequence: `\\u`")
+
+	_, err = regexp.Compile(`^[a-zA-Z\x{0080}-\x{024F}]+$`)
+	require.NoError(t, err)
+
+	require.Equal(t, `^[a-zA-Z\x{0080}-\x{024F}]+$`, intoGoRegexp(`^[a-zA-Z\u0080-\u024F]+$`))
+}
