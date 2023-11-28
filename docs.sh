1317603d08070dfe215753b53fbc26bd46c806d9
Pierre Fenoll <pierrefenoll@gmail.com>
2023-11-28 09:46:41 +0100

    docs.sh: fix narrow docs checks spectrum
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- docs.sh
+++ docs.sh
@@ -4,7 +4,7 @@ set -o pipefail
 outdir=.github/docs
 mkdir -p "$outdir"
 for pkgpath in $(git ls-files | grep  / | while read -r path; do dirname "$path"; done | sort -u | grep -vE '[.]git|testdata|cmd/'); do
-	go doc -short "./$pkgpath" | tee "$outdir/${pkgpath////_}.txt"
+	go doc -all ./"$pkgpath" | tee "$outdir/${pkgpath////_}.txt"
 done
 
 git --no-pager diff -- .github/docs/
