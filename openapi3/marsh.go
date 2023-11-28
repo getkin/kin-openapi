f6d1b8c132b1ba26fe4054238278b1c0c016248f
Pierre Fenoll <pierrefenoll@gmail.com>
2023-11-28 11:51:16 +0100

    fix after #870: make sure Bis does not surface up (#878)

--- openapi3/marsh.go
+++ openapi3/marsh.go
@@ -9,9 +9,9 @@ import (
 )
 
 func unmarshalError(jsonUnmarshalErr error) error {
-	if before, after, found := strings.Cut(jsonUnmarshalErr.Error(), "Bis."); found && before != "" && after != "" {
+	if before, after, found := strings.Cut(jsonUnmarshalErr.Error(), "Bis"); found && before != "" && after != "" {
 		before = strings.ReplaceAll(before, " Go struct ", " ")
-		return fmt.Errorf("%s.%s", before, after)
+		return fmt.Errorf("%s%s", before, strings.ReplaceAll(after, "Bis", ""))
 	}
 	return jsonUnmarshalErr
 }
