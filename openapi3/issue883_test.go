1d77b265acbf902e46c506832db9ff7d92494cb9
Pierre Fenoll <pierrefenoll@gmail.com>
2023-12-11 11:55:21 +0100

    try harder
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- openapi3/issue883_test.go
+++ openapi3/issue883_test.go
@@ -3,8 +3,9 @@ package openapi3_test
 import (
 	"testing"
 
-	"github.com/invopop/yaml"
+	invopopYaml "github.com/invopop/yaml"
 	"github.com/stretchr/testify/require"
+	v3 "gopkg.in/yaml.v3"
 
 	"github.com/getkin/kin-openapi/openapi3"
 )
@@ -19,11 +20,13 @@ info:
     name: MIT
 servers:
 - url: http://petstore.swagger.io/v1
-paths: {}
-components:
-  schemas:
-    Kitten:
-      type: string
+paths:
+  /simple:
+    get:
+      summary: A simple GET request
+      responses:
+        '200':
+          description: OK
 `[1:]
 
 	sl := openapi3.NewLoader()
@@ -35,27 +38,65 @@ components:
 	require.NoError(t, err)
 	require.NotNil(t, doc.Paths)
 
-	marshalledJson, err := doc.MarshalJSON()
-	require.NoError(t, err)
-	require.JSONEq(t, `{
-  "openapi": "3.0.0",
-  "info": {
-    "version": "1.0.0",
-    "title": "Swagger Petstore",
-    "license": {"name": "MIT"}
-  },
-  "servers": [{"url": "http://petstore.swagger.io/v1"}],
-  "paths": {},
-  "components": {
-    "schemas": {
-      "Kitten": {"type": "string"}
-    }
-  }
-}`, string(marshalledJson))
-	require.NotNil(t, doc.Paths)
+	t.Run("Roundtrip invopop/yaml", func(t *testing.T) {
+		justPaths, err := invopopYaml.Marshal(doc.Paths)
+		require.NoError(t, err)
+		require.NotNil(t, doc.Paths)
+		require.YAMLEq(t, `
+/simple:
+  get:
+    summary: A simple GET request
+    responses:
+      '200':
+        description: OK
+`[1:], string(justPaths))
 
-	marshalledYaml, err := yaml.Marshal(&doc)
-	require.NoError(t, err)
-	require.YAMLEq(t, spec, string(marshalledYaml))
-	require.NotNil(t, doc.Paths)
+		marshalledYaml, err := invopopYaml.Marshal(doc)
+		require.NoError(t, err)
+		require.NotNil(t, doc.Paths)
+		require.YAMLEq(t, spec, string(marshalledYaml))
+
+		var newDoc openapi3.T
+		err = invopopYaml.Unmarshal(marshalledYaml, &newDoc)
+		require.NoError(t, err)
+		require.NotNil(t, newDoc.Paths)
+		require.Equal(t, doc, &newDoc)
+	})
+
+	t.Run("Roundtrip yaml.v3", func(t *testing.T) {
+		justPaths, err := doc.Paths.MarshalJSON()
+		require.NoError(t, err)
+		require.NotNil(t, doc.Paths)
+		require.YAMLEq(t, `
+/simple:
+  get:
+    summary: A simple GET request
+    responses:
+      '200':
+        description: OK
+`[1:], string(justPaths))
+
+		justPaths, err = v3.Marshal(doc.Paths)
+		require.NoError(t, err)
+		require.NotNil(t, doc.Paths)
+		require.YAMLEq(t, `
+/simple:
+  get:
+    summary: A simple GET request
+    responses:
+      '200':
+        description: OK
+`[1:], string(justPaths))
+
+		marshalledYaml, err := v3.Marshal(doc)
+		require.NoError(t, err)
+		require.NotNil(t, doc.Paths)
+		require.YAMLEq(t, spec, string(marshalledYaml))
+
+		var newDoc openapi3.T
+		err = v3.Unmarshal(marshalledYaml, &newDoc)
+		require.NoError(t, err)
+		require.NotNil(t, newDoc.Paths)
+		require.Equal(t, doc, &newDoc)
+	})
 }
