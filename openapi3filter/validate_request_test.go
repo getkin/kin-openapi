61e59683bc0920416244c91b128093a324590143
Daniel Castro <danicc097@gmail.com>
2024-03-12 18:50:59 +0100

    remove duplicate parse test

--- openapi3filter/validate_request_test.go
+++ openapi3filter/validate_request_test.go
@@ -341,18 +341,6 @@ func TestValidateQueryParams(t *testing.T) {
 				},
 			},
 		},
-		{
-			name: "deepObject explode additionalProperties with object properties - missing index on nested array",
-			param: &openapi3.Parameter{
-				Name: "param", In: "query", Style: "deepObject", Explode: explode,
-				Schema: objectOf(
-					"obj", additionalPropertiesObjectOf(objectOf("item1", integerSchema, "item2", stringArraySchema)),
-					"objIgnored", objectOf("items", stringArraySchema),
-				),
-			},
-			query: "param[obj][prop2][item2]=def",
-			err:   &ParseError{path: []interface{}{"obj", "prop2", "item2"}, Kind: KindInvalidFormat, Reason: "array items must be set with indexes"},
-		},
 	}
 
 	for _, tc := range testCases {
