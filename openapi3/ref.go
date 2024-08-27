3aa1f029a87a13e8c4817a72fcb25f7c74ab507b
Ian Rees <ian@ianrees.net>
2024-08-27 03:36:43 -0700

    Possible fix for failing test that checks for validaiton methods

--- openapi3/ref.go
+++ openapi3/ref.go
@@ -1,5 +1,9 @@
 package openapi3
 
+import (
+	"context"
+)
+
 //go:generate go run refsgenerator.go
 
 // Ref is specified by OpenAPI/Swagger 3.0 standard.
@@ -7,4 +11,10 @@ package openapi3
 type Ref struct {
 	Ref        string         `json:"$ref" yaml:"$ref"`
 	Extensions map[string]any `json:"extensions,omitempty" yaml:"$ref,omitempty"`
+}
+
+// Validate returns an error if Extensions does not comply with the OpenAPI spec.
+func (e *Ref) Validate(ctx context.Context, opts ...ValidationOption) error {
+	ctx = WithValidationOptions(ctx, opts...)
+	return validateExtensions(ctx, e.Extensions)
 }
