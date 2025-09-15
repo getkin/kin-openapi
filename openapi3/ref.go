c3bd851090287147379ed37ce4e8bacfb3ed988c
Ian Rees <ian@ianrees.net>
2025-09-15 14:15:15 -0700

    Fix merge

--- openapi3/ref.go
+++ openapi3/ref.go
@@ -12,6 +12,7 @@ import (
 type Ref struct {
 	Ref        string         `json:"$ref" yaml:"$ref"`
 	Extensions map[string]any `json:"-" yaml:"-"`
+	Origin     *Origin        `json:"__origin__,omitempty" yaml:"__origin__,omitempty"`
 }
 
 // MarshalYAML returns the YAML encoding of Ref.
