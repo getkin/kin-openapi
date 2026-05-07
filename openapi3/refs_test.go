8530eb545ff73055052150fd3605b1125d36b212
Pierre Fenoll <pierrefenoll@gmail.com>
2026-05-07 13:07:27 +0200

    fixup tmpl
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- openapi3/refs_test.go
+++ openapi3/refs_test.go
@@ -5,9 +5,10 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/getkin/kin-openapi/openapi3"
 )
 
 func TestCallbackRef_Extensions(t *testing.T) {
