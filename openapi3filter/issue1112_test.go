28e3b7852ddd4f4329769a41d82a6c508522e2d0
Berserk-hub150 <89993099+Berserk-hub150@users.noreply.github.com>
2026-08-18 00:26:30 +0200

    04/17 openapi3filter: reproduce issue 1112 free-form deepObject
    Signed-off-by: Berserk-hub150 <89993099+Berserk-hub150@users.noreply.github.com>

---
+++ openapi3filter/issue1112_test.go
@@ -0,0 +1,57 @@
+package openapi3filter
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/getkin/kin-openapi/openapi3"
+)
+
+func issue1112ObjectSchema(additional openapi3.AdditionalProperties) *openapi3.SchemaRef {
+	return &openapi3.SchemaRef{
+		Value: &openapi3.Schema{
+			Type:                 &openapi3.Types{"object"},
+			Properties:           make(map[string]*openapi3.SchemaRef),
+			AdditionalProperties: additional,
+		},
+	}
+}
+
+func issue1112Decode(t *testing.T, paramName, rawQuery string, schema *openapi3.SchemaRef) (any, bool, error) {
+	t.Helper()
+
+	req, err := http.NewRequest(http.MethodGet, "http://example.test/?"+rawQuery, nil)
+	require.NoError(t, err)
+
+	param := &openapi3.Parameter{
+		Name:    paramName,
+		In:      openapi3.ParameterInQuery,
+		Style:   "deepObject",
+		Explode: openapi3.Ptr(true),
+		Schema:  schema,
+	}
+
+	return decodeStyledParameter(param, &RequestValidationInput{Request: req})
+}
+
+func TestIssue1112FreeFormDeepObject(t *testing.T) {
+	schema := issue1112ObjectSchema(openapi3.AdditionalProperties{Has: openapi3.Ptr(true)})
+
+	value, found, err := issue1112Decode(
+		t,
+		"properties",
+		"properties[vaccinated]=true&properties[color]=black&properties[coat_length]=large",
+		schema,
+	)
+
+	require.NoError(t, err)
+	require.True(t, found)
+	require.Equal(t, map[string]any{
+		"vaccinated":  "true",
+		"color":       "black",
+		"coat_length": "large",
+	}, value)
+	require.NoError(t, schema.Value.VisitJSON(value))
+}
